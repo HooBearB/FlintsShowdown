986504cd08f24e1e0513bd20a544e8b3dffcb3a1
Flint <nbeeson708891@gmail.com>
2022-01-23 20:28:08 -0600

    Initial commit

---
+++ FlintsShowdown.py
@@ -0,0 +1,8 @@
+#FLINT'S SHOWDOWN
+"""
+A fun little project for me to do,
+this is kinda like those hunger games
+simulations you can find online and
+put all your friends into.
+"""
+#Project start: Jan 23, 2022
